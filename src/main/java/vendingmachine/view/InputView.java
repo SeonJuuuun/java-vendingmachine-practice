fbdf338d239d280027492a2618dd0d95b1671211
yuseonjun <seonjun0906@gmail.com>
2023-08-26 17:58:56 +0900

    feat: 자판 보유 금액 입력 메서드 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,5 +1,17 @@
 package vendingmachine.view;
 
+import static vendingmachine.view.InputValidator.validateInputPrice;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
 
+    private static final String ASK_VENDING_MACHINE_PRICE = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+
+    public int inputVendingMachinePrice() {
+        System.out.println(ASK_VENDING_MACHINE_PRICE);
+        String price = Console.readLine();
+        validateInputPrice(price);
+        return Integer.parseInt(price);
+    }
 }
