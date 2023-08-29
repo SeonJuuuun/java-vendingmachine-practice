5e691f87e58a4058ef68358f27fda7bb612e8b06
yuseonjun <seonjun0906@gmail.com>
2023-08-29 16:33:25 +0900

    feat: 컨트롤러 대략적인 기능 구현

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,24 +1,50 @@
 package vendingmachine.controller;
 
+import java.util.List;
 import vendingmachine.domain.CoinGroup;
 import vendingmachine.domain.Money;
+import vendingmachine.domain.Product;
+import vendingmachine.domain.Products;
+import vendingmachine.domain.VendingMachine;
 import vendingmachine.view.InputView;
 import vendingmachine.view.OutputView;
 
 public class VendingMachineController {
 
-    private final CoinGroup coinGroup;
-
-    public VendingMachineController(CoinGroup coinGroup) {
-        this.coinGroup = coinGroup;
-    }
-
     public void play() {
-        Money money = insertMoney();
-        OutputView.printMap(coinGroup.separateCoin(money));
+        start();
     }
 
-    public Money insertMoney() {
-       return new Money(InputView.inputVendingMachinePrice());
+    private void start() {
+        Money money = vendingMachineMoney();
+        CoinGroup coinGroup = new CoinGroup();
+        OutputView.printMap(coinGroup.separateCoin(money));
+        Products products = new Products(insertProduct());
+        VendingMachine vendingMachine = new VendingMachine(products, insertPrice());
+        soldOutOrEmptyPrice(vendingMachine);
+    }
+
+    private void soldOutOrEmptyPrice(VendingMachine vendingMachine) {
+        while (!vendingMachine.isSoldOut()) {
+            OutputView.printPrice(vendingMachine.getPrice());
+            String productName = inputProductName();
+            vendingMachine.purchaseProduct(productName);
+        }
+    }
+
+    private String inputProductName() {
+        return InputView.inputProductName();
+    }
+
+    private Money vendingMachineMoney() {
+        return new Money(InputView.inputVendingMachinePrice());
+    }
+
+    private List<Product> insertProduct() {
+        return InputView.inputProducts();
+    }
+
+    private int insertPrice() {
+        return InputView.inputPrice();
     }
 }
