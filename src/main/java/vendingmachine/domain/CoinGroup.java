ebad4bc0291e5b86583fb2b1d8ebcaffa16f55f7
yuseonjun <seonjun0906@gmail.com>
2023-08-29 16:31:29 +0900

    feat: 동전 내림차순 기능 추가

--- src/main/java/vendingmachine/domain/CoinGroup.java
+++ src/main/java/vendingmachine/domain/CoinGroup.java
@@ -1,13 +1,23 @@
 package vendingmachine.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CoinGroup {
 
     public final Map<Coin, Integer> coins = new HashMap<>();
-    
+
+    public CoinGroup() {
+        initMap();
+    }
+
     private void initMap() {
         for (Coin coin : Coin.values()) {
             coins.put(coin, 0);
@@ -15,7 +25,6 @@ public class CoinGroup {
     }
 
     public Map<Coin, Integer> separateCoin(Money money) {
-        initMap();
         while (!money.isEmpty()) {
             Coin coin = Coin.getCoin(Randoms.pickNumberInList(Coin.toList()));
             if (money.isBiggerOrSame(coin.getAmount())) {
@@ -23,6 +32,19 @@ public class CoinGroup {
                 money.spend(coin.getAmount());
             }
         }
-        return coins;
+        return sortCoinGroup(coins);
+    }
+
+    private LinkedHashMap<Coin, Integer> sortCoinGroup(Map<Coin, Integer> map) {
+        return map.entrySet().stream()
+            .sorted(
+                Map.Entry.<Coin, Integer>comparingByKey(Comparator.comparingInt(Coin::getAmount))
+                    .reversed())
+            .collect(Collectors.toMap(
+                Map.Entry::getKey,
+                Map.Entry::getValue,
+                (oldValue, newValue) -> oldValue,
+                LinkedHashMap::new
+            ));
     }
 }
