632a2ddeeae669014cb52b8db3805ec9e3e94791
yuseonjun <seonjun0906@gmail.com>
2023-08-29 17:11:12 +0900

    feat: 투입 금액과 잔돈 반환 출력 메서드 추가

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,7 +1,10 @@
 package vendingmachine.view;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 import vendingmachine.domain.Coin;
+import vendingmachine.domain.CoinGroup;
+import vendingmachine.domain.VendingMachine;
 
 public class OutputView {
 
@@ -13,4 +16,33 @@ public class OutputView {
             System.out.println(coin.getAmount() + "원 - " + coins.get(coin) + "개");
         }
     }
+
+    public static void printPrice(int price) {
+        System.out.println("투입 금액 : " + price);
+    }
+
+//    public static void printSoldOut(String productName) {
+//        System.out.println("입력하신 상품의 재고가 없습니다.");
+//    }
+
+    public static void printRemainingCoin(VendingMachine vendingMachine, CoinGroup coinGroup) {
+        System.out.println("잔돈");
+
+        int remainingChange = vendingMachine.getPrice();
+        Map<Coin, Integer> remainingCoins = new LinkedHashMap<>(coinGroup.getCoins());
+
+        for (Map.Entry<Coin, Integer> entry : remainingCoins.entrySet()) {
+            Coin coin = entry.getKey();
+            int coinCount = entry.getValue();
+            int numCoinsToReturn = remainingChange / coin.getAmount();
+            int numCoinsReturned = Math.min(numCoinsToReturn, coinCount);
+
+            if (numCoinsReturned > 0) {
+                System.out.println(coin.getAmount() + "원 - " + numCoinsReturned + "개");
+                remainingChange -= numCoinsReturned * coin.getAmount();
+            }
+        }
+    }
+
+
 }
