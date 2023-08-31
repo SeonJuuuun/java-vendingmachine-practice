a82fb911ddaf6066fd35dbc7d954b991c8c3611d
yuseonjun <seonjun0906@gmail.com>
2023-08-31 15:25:06 +0900

    feat: 상품 예외처리 메서드 추가

--- src/main/java/vendingmachine/domain/Product.java
+++ src/main/java/vendingmachine/domain/Product.java
@@ -4,9 +4,12 @@ public class Product {
 
     private final String name;
     private final int price;
+
     private int quantity;
 
     public Product(String name, int price, int quantity) {
+        validatePrice(price);
+        validateQuantity(quantity);
         this.name = name;
         this.price = price;
         this.quantity = quantity;
@@ -20,15 +23,37 @@ public class Product {
         return price;
     }
 
-    public int getQuantity() {
-        return quantity;
-    }
-
     public boolean isSoldOut() {
         return this.quantity == 0;
     }
 
     public void decreaseQuantity() {
         this.quantity--;
+    }
+
+    private void validateQuantity(int quantity) {
+        checkInteger(quantity);
+    }
+
+    private void validatePrice(int price) {
+        checkInteger(price);
+        checkPriceRange(price);
+    }
+
+    private void checkInteger(int input) {
+        try {
+            Integer.valueOf(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 입력은 숫자만 가능합니다.");
+        }
+    }
+
+    private void checkPriceRange(int input) {
+        if (input < 100) {
+            throw new IllegalArgumentException("[ERROR] 상품 가격은 100원부터 시작합니다.");
+        }
+        if (!(input % 10 == 0)) {
+            throw new IllegalArgumentException("[ERROR] 상품 가격은 10원으로 나누어 떨어져야 합니다.");
+        }
     }
 }
