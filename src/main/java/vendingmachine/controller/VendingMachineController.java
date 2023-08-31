01121cf9d7bbf111d5b06e6e63e538fb7bf22549
yuseonjun <seonjun0906@gmail.com>
2023-08-31 15:24:43 +0900

    refactor: 예외가 터지면 다시 시작하는 기능 추가

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -16,36 +16,62 @@ public class VendingMachineController {
     }
 
     private void start() {
-        Money money = vendingMachineMoney();
+        Money money = inputVendingMachineMoney();
         CoinGroup coinGroup = new CoinGroup();
         OutputView.printMap(coinGroup.separateCoin(money));
         Products products = new Products(insertProduct());
         VendingMachine vendingMachine = new VendingMachine(products, insertPrice());
-        soldOutOrEmptyPrice(vendingMachine);
-        OutputView.printRemainingCoin(vendingMachine,coinGroup);
+        vendingMachineCalculate(vendingMachine);
+        OutputView.printPrice(vendingMachine.getPrice());
+        OutputView.printRemainingCoin(vendingMachine, coinGroup);
     }
 
-    private void soldOutOrEmptyPrice(VendingMachine vendingMachine) {
-        while (!vendingMachine.isSoldOut() && vendingMachine.priceOfRemainingProduct()) {
+    private void vendingMachineCalculate(VendingMachine vendingMachine) {
+        while (!vendingMachine.priceOfRemainingProductPrice()) {
             OutputView.printPrice(vendingMachine.getPrice());
             String productName = inputProductName();
+            vendingMachine.soldOut(productName);
             vendingMachine.purchaseProduct(productName);
         }
     }
 
     private String inputProductName() {
-        return InputView.inputProductName();
+        while (true) {
+            try {
+                return InputView.inputProductName();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
-    private Money vendingMachineMoney() {
-        return new Money(InputView.inputVendingMachinePrice());
+    private Money inputVendingMachineMoney() {
+        while (true) {
+            try {
+                return new Money(InputView.inputVendingMachinePrice());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     private List<Product> insertProduct() {
-        return InputView.inputProducts();
+        while (true) {
+            try {
+                return InputView.inputProducts();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 
     private int insertPrice() {
-        return InputView.inputPrice();
+        while (true) {
+            try {
+                return InputView.inputPrice();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
