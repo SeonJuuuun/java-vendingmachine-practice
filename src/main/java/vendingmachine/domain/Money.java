12d68d685d556f9ac01662b9b16832779aea8930
yuseonjun <seonjun0906@gmail.com>
2023-08-31 15:25:47 +0900

    feat: 자판기 금액 예외처리 메서드 추가

--- src/main/java/vendingmachine/domain/Money.java
+++ src/main/java/vendingmachine/domain/Money.java
@@ -5,11 +5,8 @@ public class Money {
     private int money;
 
     public Money(int money) {
+        validateMoney(money);
         this.money = money;
-    }
-
-    public int getMoney() {
-        return money;
     }
 
     public void spend(int money) {
@@ -22,5 +19,24 @@ public class Money {
 
     public boolean isEmpty() {
         return money == 0;
+    }
+
+    private void validateMoney(int money) {
+        checkInteger(money);
+        checkMoneyRange(money);
+    }
+
+    private void checkInteger(int input) {
+        try {
+            Integer.valueOf(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 입력은 숫자만 가능합니다.");
+        }
+    }
+
+    private void checkMoneyRange(int input) {
+        if (input < 10) {
+            throw new IllegalArgumentException("[ERROR] 자판기의 동전은 10원이상 입력 가능합니다.");
+        }
     }
 }
