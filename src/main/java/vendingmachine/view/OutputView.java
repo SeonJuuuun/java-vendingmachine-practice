91720cb4f42db444ecf864d671e8fe661283c4c0
yuseonjun <seonjun0906@gmail.com>
2023-08-31 15:26:08 +0900

    feat: 잔돈 기능 메서드 추가

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -8,41 +8,39 @@ import vendingmachine.domain.VendingMachine;
 
 public class OutputView {
 
-    private static final String HAVE_COIN = "자판기가 보유한 동전";
+    private static final String LINE_BREAK = "\n";
 
     public static void printMap(Map<Coin, Integer> coins) {
-        System.out.println(HAVE_COIN);
+        System.out.print(LINE_BREAK);
+        System.out.println("자판기가 보유한 동전");
         for (Coin coin : coins.keySet()) {
             System.out.println(coin.getAmount() + "원 - " + coins.get(coin) + "개");
         }
+        System.out.print(LINE_BREAK);
     }
 
     public static void printPrice(int price) {
-        System.out.println("투입 금액 : " + price);
+        System.out.print(LINE_BREAK);
+        System.out.println("투입 금액 : " + price + "원");
     }
-
-//    public static void printSoldOut(String productName) {
-//        System.out.println("입력하신 상품의 재고가 없습니다.");
-//    }
 
     public static void printRemainingCoin(VendingMachine vendingMachine, CoinGroup coinGroup) {
         System.out.println("잔돈");
-
         int remainingChange = vendingMachine.getPrice();
         Map<Coin, Integer> remainingCoins = new LinkedHashMap<>(coinGroup.getCoins());
+        printCoinNumber(remainingChange, remainingCoins);
+    }
 
+    private static void printCoinNumber(int remainingChange, Map<Coin, Integer> remainingCoins) {
         for (Map.Entry<Coin, Integer> entry : remainingCoins.entrySet()) {
             Coin coin = entry.getKey();
             int coinCount = entry.getValue();
             int numCoinsToReturn = remainingChange / coin.getAmount();
             int numCoinsReturned = Math.min(numCoinsToReturn, coinCount);
-
             if (numCoinsReturned > 0) {
                 System.out.println(coin.getAmount() + "원 - " + numCoinsReturned + "개");
                 remainingChange -= numCoinsReturned * coin.getAmount();
             }
         }
     }
-
-
 }
